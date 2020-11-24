4427f90d5c4940036b3a37ccb6d0e8df735dbaad
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-24 09:32:40 -0800

    added registration auth

--- server/authController.js
+++ server/authController.js
@@ -1,10 +1,30 @@
-/**
- * Auth Controller
- */
+/* Auth Controller*/
+const bcrypt = require('bcrypt');
 
 module.exports = {
-  register: (req, res) => {},
-  login: (req, res) => {},
-  logout: (req, res) => {}
+  register: async (req, res) => {
+    const db = req.app.get('db');
+    const {firstName, lastName, email, password} = req.body
+    try{
+      const [foundUser] = await db.find_email(email)
+      if(foundUser){
+        res.status(400).send('user already exists with this email')
+      } else {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password,salt);
+        const [newUser] = await db.register_user([email, hash])
+        req.session.user = newUser;
+        res.status(200).send(req.session.user)
+      }
+    } catch(err){
+      'database error on registration function', err
+    }
+  },
+  login: (req, res) => {
+
+  },
+  logout: (req, res) => {
+
+  }
 
 }
