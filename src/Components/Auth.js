686ef73efe9bf6011ea4eba76037451f27c3bf6f
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-27 11:22:03 -0800

    got rid of web vitals

---
+++ src/Components/Auth.js
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+
+const Auth = () => {
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <div>
+      <h1>hello from auth</h1>
+    </div>
+  )
+}
+
+export default Auth;
