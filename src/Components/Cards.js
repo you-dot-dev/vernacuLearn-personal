33debf4c454b5ea0febfe1e10e530b96f938298b
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-27 15:35:36 -0800

    getting somewhere

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,7 +6,16 @@ const Cards = () => {
 
   return (
     <div>
-      <h1>hello from Cards</h1>
+      <h1 id='grocery-list-title'></h1>
+      <h3 id='grocery-status'></h3>
+      <div class="grocery-list-container">
+    <div class="red-line"></div>
+    <div class="input-container">
+      <input type="text" id='item' />
+      <button id='btn'>Add Item</button>
+    </div>
+    <ul id='grocery-list'></ul>
+    </div>
     </div>
   )
 }
