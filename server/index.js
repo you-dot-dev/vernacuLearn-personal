a900eb49ab00ab8dd2eb369bc40f5d8c12c57b79
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-27 18:07:14 -0800

    endpoints

--- server/index.js
+++ server/index.js
@@ -32,6 +32,17 @@ massive({
 }).catch(err => console.log('database error' + err));
 
 
+//endpoints
+app.get('/auth/login', ctrl.login)
+app.get('/auth/register', ctrl.register)
+app.get('/api/cards', ctrl.getAllCards)
+app.get('/api/cards/:id', ctrl.getOneCard)
+app.get('/api/cards/:id', ctrl.getOneCard)
+app.post('api/cards/:id', ctrl.addCard)
+app.put('api/cards/:id', ctrl.editCard)
+app.delete('api/cards/:id', ctrl.deleteCard)
+
+
 app.listen(SERVER_PORT, console.log(`listening on port ${SERVER_PORT}`))
 
 
