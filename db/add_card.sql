3eee8de00e870e07d73766120a7074f092108452
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-27 18:13:31 -0800

    database queries

---
+++ db/add_card.sql
@@ -0,0 +1,4 @@
+INSERT INTO cards 
+(word_or_phrase, definition, difficulty, category)
+VALUES
+($1, $2, $3, $4)
---
+++ db/add_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, first_name, last_name, password)
+VALUES
+($1, $2, $3, $4)
