409ed3455c333fc79f2ae30f7b50b95b9ff4fd03
Paul Coe <paul@pauls-macbook.local>
2020-12-31 13:07:35 -0800

    Add initial markup for Homepage component

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,11 +1,29 @@
 import logo from '../img/vernacuLearn.png';
+import facelessPeople from '../img/faceless-people.jpg';
 
 
 const HomePage = () => {
   return(
     <div>
-      <h1>homepage</h1>
-      <img src={logo} alt="vernacuLearn Logo"/>
+
+      <header>
+        <img src={logo} className="logo" />
+        <span>asdf</span>
+      </header>
+
+      <h1>Learn essential lingo fast!</h1>
+
+      <main>
+        <section className="hero-container">
+          <section className="sign-in">
+            <h1>Already a member? Sign In!</h1>
+          
+          </section>
+          <section className="hero-image">
+          </section>
+        </section>
+      </main>
+
     </div>
   )
 }
